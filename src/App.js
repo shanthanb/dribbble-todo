e7bdc251f71f8161520c790e78b9f8c6ec8f6f49
shanthanb <shanthanboppudi@gmail.com>
2022-11-21 17:35:09 +0530

    todo-dribble

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
 import List from './List'
 import Header from './Header';
-import Footer from './Footer';
 
 const Lists = [{
-
+  // logo: "img",
   iconColor: "#a90000",
   title: "Organize photoshoot",
   tasks: ["Work", "Hooli"],
-  day: "Today"
+  day: "Today",
+  isCompleted: true
 },
 
 {
   iconColor: "#99FFCC",
   title: "Buy Milk",
   tasks: ["Grocery"],
-  day: "Today"
+  day: "Today",
+  isCompleted: true
 
 },
 {
   iconColor: "red",
   title: "Setup meeting w/ Globex",
   tasks: ["Work"],
-  day: "Today"
+  day: "Today",
+  isCompleted: true
+
 },
 {
   iconColor: "purple",
   title: "Renew passport",
   tasks: ["Personal"],
-  day: "Tomorrow"
+  day: "Tomorrow",
+  isCompleted: true
+
 },
 
 {
   iconColor: "red",
   title: "Checklist LA Trip",
   tasks: ["Work"],
-  day: "Tomorrow"
+  day: "Tomorrow",
+  isCompleted: true
+
 }
 
 ]
 const App = () => {
-
   return (
 
     <div>
@@ -50,7 +56,18 @@ const App = () => {
             <List key={i} iconColor={list.iconColor} title={list.title} tasks={list.tasks} day={list.day} />)
         })
       }
-      <Footer />
+      <h1 style={{ marginLeft: "1rem", fontSize: "25px", fontFamily: "Poppins,sans-serif", fontWeight: "300" }}>Completed</h1>
+      {Lists.map((list, i) => {
+        return (
+          <List key={i}
+            iconColor={list.iconColor}
+            isCompleted={list.isCompleted}
+            title={list.title}
+            tasks={list.tasks}
+            day={list.day} />
+        )
+      })
+      }
     </div>
   );
 }
--- src/List.js
+++ src/List.js
@@ -1,27 +1,37 @@
 import React from 'react'
 import './List.css'
-const List = ({ iconColor, title, tasks, day }) => {
+import { MdCheckCircle } from "react-icons/md";
+
+
+const List = ({ iconColor, title, tasks, day, isCompleted }) => {
 
 
     return (
 
         <div className='list-items'>
             <div className='items-left'>
-                <div className='round-icon' style={{ border: "2.5px solid " + iconColor }} />
+                {isCompleted ? <MdCheckCircle style={{ marginRight: "1.3rem", height: "27px", width: "27px", color: "rgb(91,97,122)" }} />
+
+                    : <div className='round-icon' style={{ border: "2.5px solid " + iconColor }} />
+
+                }
                 <div className='left-title'>
-                    <h5>{title}</h5>
-                    <span>{day}</span>
+
+                    <h5 style={isCompleted ? { textDecoration: "line-through", color: "lightgray" } : {}}>{title}</h5>
+                    <span style={isCompleted ? { textDecoration: "line-through", color: "lightgray" } : {}}>{day}</span>
+
+                    {/* <h5 style={isCompleted && { textDecoration: "line-through", color: "gray" }}>{title}</h5>
+                    <span style={isCompleted && { textDecoration: "line-through", color: "gray" }}>{day}</span> */}
                 </div>
             </div>
             <div className='items-right'>
-                {tasks.map((task) => {
-
+                {tasks.map((task, i) => {
                     return (
-                        <span>
+                        <span style={isCompleted ? { backgroundColor: "hsl(158, 3%, 93%)", color: "gray" } : {}} key={i}>
                             {task}
+
                         </span>
                     )
-
                 })}
             </div>
         </div>
